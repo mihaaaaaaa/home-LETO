e4db4e5f161a29edf021b881843eb2b842df4409
mihaaaaaaa <mihakruts@yandex.ru>
2018-08-28 18:36:48 +0400

    4

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -4,6 +4,8 @@ $(document).ready(function() {
 
 	let v = 0;
 
+	var questionText = ('Вам хотелось бы создавать игры ?');
+
 	let questions = [
 			'.1',
 			'.2',
@@ -21,28 +23,45 @@ $(document).ready(function() {
 			'.14'
 		];
 
+		function changeText(){
+			document.getElementById('question').innerHTML = questionText;
+		}
+
+		function chengeAnswer(){
+			document.getElementById('answer-1').innerHTML = ('да');
+			document.getElementById('answer-2').innerHTML = ('нет');
+		}
+
+		chengeAnswer();
+		changeText();
+
 		$('.answer-1').click(function(){
-			questions[v] = true;
+			questions[v] = true ;
 			alert(questions);
 			v = v + 1;
-			
-		})
+		});
 
 		$('.answer-2').click(function(){
 			questions[v] = false;
 			alert(questions);
 			v = v + 1;
-		})
+		});
 
 		if (questions[0] == true) {
-			$(".question").innerHTML="Новый текст!";
+			questionText = ('Вам больше нравится визуальная часть игры или интересно узнать о том , как в играх можно применять программирование?');
+			changeText()
+			document.getElementById('answer-1').innerHTML = ('если код');
+			document.getElementById('answer-2').innerHTML = ('если визуалка');
 		}
 
 		if (questions[0] == true && questions[1] == true)  {
-			alert(2);
+			questionText = ('Вам больше тринадцати ?');
+			changeText();
+			chengeAnswer();
 		}
 
 		if (questions[0] == true && questions[1] == true && questions[2] == true) {
-			alert(3);
+			questionText = ('Ваша рабочая среда : C#');
+
 		}
 });
